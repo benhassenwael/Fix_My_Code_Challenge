9c9349659b82586fd3ee7e7a615f3b467afeb64e
benhassenwael <greenw2012@gmail.com>
2021-05-17 02:12:26 +0100

    -

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Square Module"""
 
 
-class square():
+class Square():
     """square class"""
     width = 0
 
@@ -25,7 +25,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.permiterOfMySquare())
